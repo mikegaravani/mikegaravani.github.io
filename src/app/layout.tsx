a1dc0e30642a39200b499feacca3ad3d79a756ca
Mike Garavani <scotchmagico@gmail.com>
2025-01-02 01:06:14 +0100

    fix syntax bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,1 @@
 import "./globals.css";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className="flex min-h-screen flex-col">
-        {/* <Header /> */}
-        {children}
-      </body>
-    </html>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex grow justify-center bg-slate-100 dark:bg-slate-800">
       <div className="container mt-[calc(10vh+40px)] flex justify-center p-4">
         <div className="flex flex-col text-center dark:text-slate-100">
-          <h1 className="text-5xl md:text-6xl">Hey, I'm Mike Garavani.</h1>
+          <h1 className="text-5xl md:text-6xl">Hey, I&apos;m Mike Garavani.</h1>
           <p className="mt-6 text-2xl md:text-3xl">
             Aspiring software engineer based in Switzerland.
           </p>
