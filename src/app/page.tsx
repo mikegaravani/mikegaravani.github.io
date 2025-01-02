bf94486622e5462740952dc2aed6479cc1cc3ab0
Mike Garavani <scotchmagico@gmail.com>
2025-01-02 17:59:31 +0100

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
-import portfolioImage from "../assets/portfolio-pic.jpeg";
+// import Image from "next/image";
+// import portfolioImage from "../assets/portfolio-pic.jpeg";
 
 export default function Home() {
   return (
-    <main className="flex grow justify-center bg-slate-100 dark:bg-slate-800">
-      <div className="container mt-[calc(10vh+40px)] flex justify-center p-4">
-        <div className="flex flex-col text-center dark:text-slate-100">
+    <main className="flex grow justify-center bg-slate-700">
+      <div className="container mx-auto mt-20 flex justify-center p-4">
+        <div className="flex flex-col text-center text-slate-200">
           <h1 className="text-5xl md:text-6xl">Hey, I&apos;m Mike Garavani.</h1>
           <p className="mt-6 text-xl md:text-2xl lg:text-3xl">
             Software developer based in Switzerland.
           </p>
           <h2 className="mb-1 mt-16 text-2xl">Projects</h2>
-          <hr className="mx-auto mb-3 w-[20px] rounded-sm border-2 border-green-300" />
+          <hr className="mx-auto w-8 rounded-sm border-2 border-green-300" />
 
           {/* PROJECT 1 */}
-          <p className="text-xl">
+          <p className="text-xl mt-3">
             <a
               href="https://github.com/mikegaravani/connect-X-project"
               className="underline"
@@ -65,8 +65,40 @@ export default function Home() {
           </p>
 
           <h2 className="mb-1 mt-16 text-2xl">Contact</h2>
-          <hr className="mx-auto mb-3 w-[20px] rounded-sm border-2 border-green-300" />
-          <p className="text-xl">michele@garavani.ch</p>
+          <hr className="mx-auto w-8 rounded-sm border-2 border-green-300" />
+
+          <p className="text-xl mt-3">
+            <a
+              href="https://www.linkedin.com/in/michelegaravani/"
+              className="underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Linkedin
+            </a>
+          </p>
+
+          <p className="text-xl">
+            <a
+              href="https://github.com/mikegaravani"
+              className="underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </p>
+
+          <p className="text-xl">
+            <a
+              href="mailto:michele@garavani.ch"
+              className="underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              michele@garavani.ch
+            </a>
+          </p>
         </div>
       </div>
     </main>
